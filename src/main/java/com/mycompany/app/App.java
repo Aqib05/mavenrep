232ea5fa37f3428048a5e2e0689b9e7b915c476b
Aqib05 <108731126+Aqib05@users.noreply.github.com>
2023-11-16 17:16:39 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -24,7 +24,7 @@ public class App extends JFrame {
     String date;
     App() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Digital Clock Project");
+        this.setTitle("Digital CLOCK");
         this.setLayout(new FlowLayout());
         this.setSize(350, 220);
         this.setResizable(false);
